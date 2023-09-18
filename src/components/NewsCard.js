027f07832e574d9d33d8c8d48ee12bb6f7411109
gsTavares <gustavo.tavares24@outlook.com>
2023-09-18 20:30:42 -0300

    feat: add style to web component

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -5,6 +5,15 @@ class NewsCard extends HTMLElement {
         // attach shadow DOM in component
         const shadow = this.attachShadow({mode: "open"});
         shadow.innerHTML = "<h1>Hello world!</h1>";
+
+        // styling components (scoped: the elements in real DOM will not be affected)
+        const style = document.createElement("style");
+        style.textContent = `
+            h1 {
+                color: red;
+            }
+        `;
+        shadow.appendChild(style);
     }
 }
 
