d0afae68645c969d47792e7df94e4594695b3b22
gsTavares <gustavo.tavares24@outlook.com>
2023-09-18 22:54:16 -0300

    feat: news card web component (React?)

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,22 +1,97 @@
 class NewsCard extends HTMLElement {
     constructor() {
         super();
+        const shadow = this.attachShadow({ mode: "open" });
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
 
-        // props
-        const newsTitle = this.getAttribute("news-title");
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
 
-        // attach shadow DOM in component
-        const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = `<h1>Hello world! "${newsTitle}"</h1>`;
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
 
-        // styling components (scoped: the elements in real DOM will not be affected)
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") ?? "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("img-src");
+        newsImage.alt = this.getAttribute("img-alt")
+
+        cardRight.appendChild(newsImage);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles() {
         const style = document.createElement("style");
         style.textContent = `
-            h1 {
-                color: red;
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                font-family: 'Segoe UI', sans-serif;
+            }
+            
+            .card {
+                width: 80%;
+                display: flex;
+                box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                -webkit-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                -moz-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                justify-content: space-between;
+                max-height: 270px;
+                margin-bottom: 1rem;
+            }
+            
+            .card__left {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+            
+            .card__left > span {
+                font-weight: 400;
+            }
+            
+            .card__left > a {
+                margin-top: 15px;
+                font-size: 25px;
+                color: black;
+                text-decoration: none;
+                font-weight: bold;
+            }
+            
+            .card__left > p {
+                color: rgb(70, 70, 70);
+            }
+            
+            .card__right img {
+                width: 100%;
+                height: 100%;
             }
         `;
-        shadow.appendChild(style);
+        return style;
     }
 }
 
